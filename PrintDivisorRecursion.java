4714668eb82b0ffab71b5cd484e6cec3f5e27489
yatheesh1989 <37588949+yatheesh1989@users.noreply.github.com>
2025-01-01 15:35:20 +0530

    Week6-print divisor using recursion
    Week6-print divisor using recursion

---
+++ PrintDivisorRecursion.java
@@ -0,0 +1,34 @@
+package DSA_Course;
+
+import java.util.ArrayList;
+
+public class PrintDivisorRecursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=12;
+		printDivr(n, 1);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		System.out.println();
+		System.out.println(printDivisor(n, 1, list));
+	}
+	
+	public static void printDivr(int n, int i) {
+		if(i<=n){
+			if(n%i == 0) {
+				System.out.print(i + " ");
+			}
+			printDivr(n, i+1);
+		}
+	}
+	
+	public static ArrayList<Integer> printDivisor(int n, int i, ArrayList<Integer> list) {
+		if(i<=n){
+			if(n%i == 0) {
+				list.add(i);
+			}
+			printDivisor(n, i+1, list);
+		}
+		return list;
+	}
+}
